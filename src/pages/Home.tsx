563d8a21428f61121e1fe4ae3e111708f59c34e1
kisagge <kisagge@naver.com>
2022-12-02 09:11:41 +0900

    fix infinite list request

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,34 @@
+import { useEffect, useState } from "react";
+
+type PostType = {
+  _id: string;
+  name: string;
+  description: string;
+};
+
 const Home = () => {
+  const [posts, setPosts] = useState<PostType[]>([]);
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch("http://localhost:4000/api/posts", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const json = await response.json();
+
+      if (response.ok) {
+        setPosts(json);
+      }
+    };
+
+    fetchPosts();
+  }, []);
+
   return (
     <div className="home">
-      <h2>home</h2>
+      <div className="posts">{posts && posts.map((post) => <p key={post._id}>{post.name}</p>)}</div>
     </div>
   );
 };
